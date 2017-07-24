7b266f1ad39fc5f5b21adfd5e61eb21111a58132
Admin <admin@Admins-Mac.Dlink>
2017-07-24 09:11:36 +0300

    carthage & fix

---
+++ Example/TimeZoneInlineRowCarthage/TimeZoneInlineRowCarthage.h
@@ -0,0 +1,19 @@
+//
+//  TimeZoneInlineRowCarthage.h
+//  TimeZoneInlineRowCarthage
+//
+//  Created by Admin on 24.07.17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeZoneInlineRowCarthage.
+FOUNDATION_EXPORT double TimeZoneInlineRowCarthageVersionNumber;
+
+//! Project version string for TimeZoneInlineRowCarthage.
+FOUNDATION_EXPORT const unsigned char TimeZoneInlineRowCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeZoneInlineRowCarthage/PublicHeader.h>
+
+
